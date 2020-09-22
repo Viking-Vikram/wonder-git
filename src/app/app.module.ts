ead6a2af7940e44e3b40bf22d7fbef99097c8ff2
kadarla vikram <kadarlavikky@gmail.com>
2020-09-22 15:04:50 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Wonder1Component } from './wonder1/wonder1.component';
+import { Wonder2Component } from './wonder2/wonder2.component';
+import { Wonder3Component } from './wonder3/wonder3.component';
+import { Wonder4Component } from './wonder4/wonder4.component';
+import { Wonder5Component } from './wonder5/wonder5.component';
+import { Wonder6Component } from './wonder6/wonder6.component';
+import { Wonder7Component } from './wonder7/wonder7.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Wonder1Component,
+    Wonder2Component,
+    Wonder3Component,
+    Wonder4Component,
+    Wonder5Component,
+    Wonder6Component,
+    Wonder7Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/wonder1/wonder1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wonder1',
+  templateUrl: './wonder1.component.html',
+  styleUrls: ['./wonder1.component.css']
+})
+export class Wonder1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
